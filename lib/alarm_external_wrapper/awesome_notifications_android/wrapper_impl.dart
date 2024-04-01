c9fde2a9829cfbdfc5f5ea4fa6e36e55df524147
jokrey <jokrey@mail>
2024-04-01 15:02:13 +0200

    removed unused import

--- lib/alarm_external_wrapper/awesome_notifications_android/wrapper_impl.dart
+++ lib/alarm_external_wrapper/awesome_notifications_android/wrapper_impl.dart
@@ -5,7 +5,6 @@ import 'package:deadlines/alarm_external_wrapper/notify_wrapper.dart';
 import 'package:deadlines/main.dart';
 import 'package:deadlines/persistence/database.dart';
 import 'package:deadlines/persistence/deadline_alarm_manager.dart';
-import 'package:deadlines/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
